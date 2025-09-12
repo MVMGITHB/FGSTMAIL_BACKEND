0fd900ba147079f6b8f2981100d1b3083f1d32a0
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-09-12 17:16:26 +0530

    url added to cors

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ connectDB();
 const app = express();
 
 const allowedOrigins = new Set([
-  "http://localhost:3000",
+   "http://localhost:3000",
   "http://localhost:5173",
-  "https://admin-daily-news.vercel.app",
-  "https://admin-daily-news.vercel.app/",
-  "http://82.25.109.68:3005",
-  "http://82.25.109.68:3005/",
-  "https://dailynewzmail.com/",
-  "https://dailynewzmail.com"
+  "https://fsadmin-ebon.vercel.app/",
+  "https://fsadmin-ebon.vercel.app",
+  "http://fgstmails.com/",
+  "http://fgstmails.com"
 
 ]);
 
